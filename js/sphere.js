84af6b52e0c63de78d859073c73fdc1667911674
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-04-01 01:48:58 -0400

    tweaks

--- js/sphere.js
+++ js/sphere.js
@@ -6,42 +6,44 @@
 (function(){
 	var d = document,
 		canvas = d.body.appendChild( d.createElement( 'canvas' ) ),
-	    context = canvas.getContext( '2d' ),
-	    time = 0,
+		context = canvas.getContext( '2d' ),
+		time = 0,
 		w = 1,
 		h = 1,
 		cos = Math.cos,
 		sin = Math.sin,
 		PI = Math.PI;
 	
-	// The below snippet can be used to make the Sphere adapt
-	// to the window size as it changes
 	function resize() {
-	    canvas.width = w = innerWidth;
-	    canvas.height = h = innerHeight;
+		canvas.width = w = innerWidth;
+		canvas.height = h = innerHeight;
 	}
-	resize();
+
+	// Monitor browser resize
 	addEventListener( 'resize', resize, false );
+
+	// Initial size
+	resize();
 	
 	// The main animation loop
-    setInterval( function() {
+	setInterval( function() {
 		context.clearRect( 0, 0, w, h );
 		context.fillStyle = 'rgba(0,255,255,.5)';
 		context.globalCompositeOperation = 'lighter';
-	    
-	    time += .1;
+		
+		time += .1;
 		
 		// The number of particles to generate
-	    i = 10000;
-	    
+		i = 10000;
+		
 		while( i-- ) {
 			// The magic
 			r = ( ( w + h ) * 0.4 ) * ( cos( ( time + i ) * ( .05 + ( ( sin(time*0.00002) / PI ) * .2 ) ) ) / PI );
-	        
-	        context.fillRect( sin(i) * r + (w/2), 
+			
+			context.fillRect( sin(i) * r + (w/2), 
 							  cos(i) * r + (h/2), 
 							  1, 
-							  1 );        
-	    }
+							  1 );
+		}
 	}, 16 );
 })()
